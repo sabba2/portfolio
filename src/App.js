c479ba844189c15fca91f833166f85729255b094
Shayan <shayandev96@gmail.com>
2024-11-07 19:35:58 +1000

    Add router and routes to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Travel from "./pages/Travel";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,7 +10,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/travel" element={<Travel />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my Portfolio</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+};
